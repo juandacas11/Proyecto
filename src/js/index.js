a32fd4ef658f7125b142bb868219a5439b59080e
sebastian <juan.castanos@udea.edu.co>
2025-03-29 08:59:39 -0500

    Carga JS

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,18 @@
-function form-container(){
-    document.getEleme
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log("DOM fully loaded and parsed");
+    const form = document.getElementById("form-input");
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        login();
+    }) 
+});
+
+function login(){
+    const form = document.getElementById("form-input");
+    const data = new FormData(form);
+    const object = {};
+    object['name'] = data.get('name');
+    object['email'] = data.get('email');
+    console.info(object);
+    console.log(object);
 }
